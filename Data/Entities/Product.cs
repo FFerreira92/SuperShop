8846efb75013b87f6aaf309c4bb5e1b0baaf16d5
FFerreira92 <F92ferreira@gmail.com>
2022-06-30 14:51:28 +0100

    Added First Controller

---
+++ Data/Entities/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperShop.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+
+        public string Name { get; set; }
+
+
+
+        // coloca formatação de preço com duas casas decimais (currency 2) mas só coloca a formatação após o input do utilizador,
+        // permitindo que o mesmo possa colocar mais do que duas casas decimais sem que ocorra erro (applyFormatInEditMode = false)
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]         
+        public decimal Price { get; set; }
+
+
+
+        [Display(Name = "Image")] // Define o nome do campo que vai surgir na página Web para esta respetiva Propriedade (só visual)
+        public string ImageUrl { get; set; }
+
+
+
+        [Display(Name = "Last Purchase")]
+        public DateTime LastPurchase { get; set; }
+
+
+
+        [Display(Name = "Last Sale")]
+        public DateTime LastSale { get; set; }
+
+        
+
+        [Display(Name = "Is Available")]
+        public bool IsAvailable { get; set; }
+
+
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+}
