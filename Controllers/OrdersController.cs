67d02ccdad60f9a32f1747c97af78d74f9968542
FFerreira92 <F92ferreira@gmail.com>
2022-07-28 14:19:42 +0100

    Add Items on detail temp.

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SuperShop.Data;
+using SuperShop.Models;
 using System.Threading.Tasks;
 
 namespace SuperShop.Controllers
@@ -9,16 +10,47 @@ namespace SuperShop.Controllers
     public class OrdersController : Controller
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly IProductsRepository _productsRepository;
 
-        public OrdersController(IOrderRepository orderRepository )
+        public OrdersController(IOrderRepository orderRepository, IProductsRepository productsRepository )
         {
             _orderRepository = orderRepository;
+            _productsRepository = productsRepository;
         }
 
 
         public async Task<IActionResult> Index()
         {
             var model = await _orderRepository.GetOrderAsync(this.User.Identity.Name);
+            return View(model);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var model = await _orderRepository.GetDetailTempsAsync(this.User.Identity.Name);
+            return View(model);
+        }
+
+        public IActionResult AddProduct()
+        {
+            var model = new AddItemViewModel
+            {
+                Quantity = 1,
+                Products = _productsRepository.GetComboProducts()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(AddItemViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _orderRepository.AddItemToOrderAsync(model,this.User.Identity.Name);
+                return RedirectToAction("Create");
+            }
+
             return View(model);
         }
     }
